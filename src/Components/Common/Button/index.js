faf6871827ec2aec01fbe8bf7215054c53072693
Jeremy Cho <hyung.cho@hyfn.com>
2017-11-14 23:49:17 -0800

    Add sample primary button

---
+++ src/Components/Common/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// CSS
+import './Button.scss'
+
+const Button = () => (
+  <div styleName='btn'>
+    Primary
+  </div>
+)
+
+export default Button
--- src/Components/Styleguide/Sections/Elements.js
+++ src/Components/Styleguide/Sections/Elements.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
 // Grid
-import { Row } from 'react-grid-system'
+import { Row, Col } from 'react-grid-system'
 
 // Components
 import SectionHeader from '../SectionHeader'
 import SubHeader from '../SubHeader'
+import Button from 'Components/Common/Button'
 
 const Elements = () => (
   <Row>
     <SectionHeader headerText='Elements' />
     <SubHeader subHeaderText='Buttons' />
+
+    <Col xs={8} offset={{ xs: 2 }}>
+      <Button />
+    </Col>
   </Row>
 )
 
