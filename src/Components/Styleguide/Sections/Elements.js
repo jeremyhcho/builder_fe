42f011232a06491d3425f263e7cc42cac8ed3dec
Grant Kang <g1k05j@gmail.com>
2017-11-15 17:12:03 -0800

    Feature: Styleguide Buttons

--- src/Components/Styleguide/Sections/Elements.js
+++ src/Components/Styleguide/Sections/Elements.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
+// Grid
+import { Row, Col } from 'react-grid-system'
+
 // Components
 import SectionHeader from '../SectionHeader'
 import SubHeader from '../SubHeader'
 import Button from 'Components/Common/Button'
 
 const Elements = () => (
-  <div style={{ marginTop: '25px' }}>
+  <Row>
     <SectionHeader headerText='Elements' />
     <SubHeader subHeaderText='Buttons' />
-    <Button />
-  </div>
+
+    <Col xs={8} offset={{ xs: 2 }}>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+      <Button flat><span style={{ textTransform: 'uppercase' }}>Flat Button</span></Button>
+    </Col>
+  </Row>
 )
 
 export default Elements
