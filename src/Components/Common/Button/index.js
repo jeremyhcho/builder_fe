b3d59aa37d0d06ee06d24ec12d6e2a2e20fd44e7
Grant Kang <g1k05j@gmail.com>
2017-11-15 17:12:03 -0800

    Feature: Styleguide Buttons

--- src/Components/Common/Button/index.js
+++ src/Components/Common/Button/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 // CSS
 import './Button.scss'
 
-const Button = () => (
-  <div styleName='btn'>
-    Primary
-  </div>
-)
+const Button = (props) => {
+  switch (true) {
+    case props.primary:
+      return <button styleName="btn primary">{props.children}</button>
+    case props.secondary:
+      return <button styleName="btn secondary">{props.children}</button>
+    case props.flat:
+      return <button styleName="btn flat">{props.children}</button>
+    default:
+      return <button styleName="btn primary">{props.children}</button>
+  }
+}
+
+Button.propTypes = {
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  flat: PropTypes.bool,
+  children: PropTypes.node
+}
+
+Button.defaultProps = {
+  primary: false,
+  secondary: false,
+  flat: false,
+  children: ''
+}
+
 
 export default Button
