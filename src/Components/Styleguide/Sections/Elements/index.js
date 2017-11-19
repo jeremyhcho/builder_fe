7e9639d2f7912af1ee600001dce1d79fa3c78e49
Jeremy Cho <hyung.cho@hyfn.com>
2017-11-18 19:00:56 -0800

    Adds Radios to new Elements/index file

--- src/Components/Styleguide/Sections/Elements/index.js
+++ src/Components/Styleguide/Sections/Elements/index.js
@@ -5,6 +5,7 @@ import SectionHeader from '../../SectionHeader'
 import SubHeader from '../../SubHeader'
 import Buttons from './Buttons'
 import Toggles from './Toggles'
+import Radios from './Radios'
 
 // CSS
 import './Elements.scss'
@@ -12,10 +13,15 @@ import './Elements.scss'
 const Elements = () => (
   <div styleName="elements">
     <SectionHeader headerText='Elements' />
+
     <SubHeader subHeaderText='Buttons' />
     <Buttons />
+
     <SubHeader subHeaderText='Toggle' />
     <Toggles />
+
+    <SubHeader subHeaderText='Radios' />
+    <Radios />
   </div>
 )
 
