71fd203c708bf04547c424a62c19ea1501b889ab
Grant Kang <g1k05j@gmail.com>
2017-11-18 18:54:14 -0800

    Refactors Elements view and Buttons/Toggles file structure

--- src/Components/Common/Button/index.js
+++ src/Components/Common/Button/index.js
@@ -1,2 +1,35 @@
-export { default as Button } from './Button'
-export { default as Toggle } from './Toggle'
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+// CSS
+import './Button.scss'
+
+const Button = ({ primary, secondary, flat, disabled, children }) => {
+  const buttonClass = classNames('btn', {
+    primary: primary || (!secondary && !flat),
+    secondary,
+    flat,
+    disabled
+  })
+
+  return <button styleName={buttonClass}>{children}</button>
+}
+
+Button.propTypes = {
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  flat: PropTypes.bool,
+  disabled: PropTypes.bool,
+  children: PropTypes.node
+}
+
+Button.defaultProps = {
+  primary: false,
+  secondary: false,
+  flat: false,
+  disabled: false,
+  children: '',
+}
+
+export default Button
