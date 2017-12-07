2a5f7e6cbf904e255abeff4500abe92e7f6c3391
Grant Kang <g1k05j@gmail.com>
2017-12-06 23:53:55 -0800

    Adds sidenav icons and layout for Sidenav component

---
+++ src/Layouts/SideNav/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+import classNames from 'classnames'
+
+// Components
+// import NavigationItem from './NavigationItem'
+import Matches from 'Assets/Icons/nav/soccer-field.svg'
+import MatchesSelected from 'Assets/Icons/nav/matches-selected.svg'
+
+// CSS
+import './SideNav.scss'
+
+const navItems = [
+  {
+    icon: Matches,
+    selectedIcon: MatchesSelected,
+    route: '/matches',
+    key: 'matches'
+  }
+]
+
+class SideNav extends React.Component {
+  state = {
+    selectedItem: ''
+  }
+
+  handleClick = (key, route) => {
+    return () => {
+      this.setState({
+        selectedItem: key
+      }, () => {
+        this.props.history.push(route)
+      })
+    }
+  }
+
+  render () {
+    return (
+      <div styleName="sidenav">
+        {navItems.map(({ icon: Icon, selectedIcon: SelectedIcon, route, key }) => {
+          const selected = key === this.state.selectedItem
+          const navItemStyle = classNames('nav-item', {
+            selected
+          })
+
+          return (
+            <div
+              key={key}
+              styleName={navItemStyle}
+              onClick={this.handleClick(key, route)}
+              selected={selected}
+            >
+              {selected ?
+                <SelectedIcon height={20} width={20} />
+                : <Icon height={20} width={20} />
+              }
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+SideNav.propTypes = {
+  history: PropTypes.object.isRequired
+}
+
+export default withRouter(SideNav)
