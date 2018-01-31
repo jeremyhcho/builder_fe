b147bcaad52746a5cf85e138d0292c7bcec735b6
Grant Kang <g1k05j@gmail.com>
2018-01-31 11:11:36 -0800

    Replaces redux devtools with extension

--- src/Config/Store.js
+++ src/Config/Store.js
@@ -1,5 +1,24 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Store.prod.js')
-} else {
-  module.exports = require('./Store.dev.js')
-}
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { routerMiddleware } from 'react-router-redux'
+import history from '../History'
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
+
+// Root Reducer
+import rootReducer from 'Reducers'
+
+// Root Saga
+import rootSaga from 'Sagas'
+
+const composeEnhancers = composeWithDevTools({})
+const sagaMiddleware = createSagaMiddleware()
+const middleware = [sagaMiddleware, routerMiddleware(history)]
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware))
+)
+
+sagaMiddleware.run(rootSaga)
+
+export default store
--- src/Router.js
+++ src/Router.js
@@ -2,19 +2,13 @@
 import React from 'react'
 import { ConnectedRouter } from 'react-router-redux'
 import history from './History'
-import DevTools from 'Config/DevTools'
 
 // Components
 import App from './App'
 
-const production = process.env.NODE_ENV === 'production'
-
 const AppRouter = () => (
   <ConnectedRouter history={history}>
-    <div>
-      <App />
-      {!production && <DevTools />}
-    </div>
+    <App />
   </ConnectedRouter>
 )
 
