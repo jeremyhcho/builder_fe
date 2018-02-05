d3d8f15d1124c51d1c915a3e04361f7ef340a3f2
Jeremy <jeremyhyungcho@gmail.com>
2018-02-04 23:00:12 -0800

    Add placeholder for no active predictions

--- src/Components/Dashboard/RecentPredictions/index.js
+++ src/Components/Dashboard/RecentPredictions/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 
 // Components
 import PredictionItem from './PredictionItem'
+import { Card } from 'Components/Common'
 
 // Actions
 import { fetchRecentPredictions } from 'Actions'
@@ -11,23 +12,49 @@ import { fetchRecentPredictions } from 'Actions'
 // CSS
 import './RecentPredictions.scss'
 
+// Icons
+import ColoredAppIcon from 'Assets/Icons/colored-app-cancel.svg'
+
 class RecentPredictions extends React.Component {
   componentDidMount () {
     this.props.fetchRecentPredictions()
   }
 
+  renderPredictions () {
+    if (!this.props.predictions.length) {
+      return (
+        <Card style={{ marginTop: 0 }} wrapperStyle={{ height: '500px', position: 'relative' }}>
+          <div
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              textAlign: 'center'
+            }}
+          >
+            <ColoredAppIcon width={100} height={100} />
+            <h4 style={{ marginTop: '15px' }}>
+              No Active Predictions Found
+            </h4>
+          </div>
+        </Card>
+      )
+    }
+
+    return this.props.predictions.map((prediction, index) => (
+      <PredictionItem
+        prediction={prediction}
+        key={prediction.id}
+        index={index}
+      />
+    ))
+  }
+
   render () {
     return (
       <div styleName='recent-predictions'>
-        {
-          this.props.predictions.map((prediction, index) => (
-            <PredictionItem
-              prediction={prediction}
-              key={prediction.id}
-              index={index}
-            />
-          ))
-        }
+        {this.renderPredictions()}
       </div>
     )
   }
