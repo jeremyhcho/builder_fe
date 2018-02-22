faa7a8ad0eff50f6e0c8acb1b7c67876a6001b0e
Grant Kang <g1k05j@gmail.com>
2018-02-21 19:16:09 -0800

    Adds footer block

--- src/Components/LandingPage/Footer/index.js
+++ src/Components/LandingPage/Footer/index.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Row, Col } from 'react-styled-flexboxgrid'
+
+// CSS
+import '../LandingPage.scss'
 
 class Footer extends React.Component {
   render () {
-    return <div />
+    return (
+      <div styleName="footer">
+        <Row center='xs'>
+          <Col xs={4} styleName="copyright">
+            <p>Copyright bullshit</p>
+          </Col>
+
+          <Col xs={4} styleName="social-media">
+            <p>Social Media link bullshit</p>
+          </Col>
+
+          <Col xs={4} styleName="contact">
+            <p>Contact address bullshit</p>
+          </Col>
+        </Row>
+      </div>
+    )
   }
 }
 
