4dea838699a37f3f2b40c7320fa10d76b974e591
Grant Kang <g1k05j@gmail.com>
2018-02-23 13:25:34 -0800

    Changes to landing page theme

--- src/Components/Settings/Blocks/SubscriptionPlan/index.js
+++ src/Components/Settings/Blocks/SubscriptionPlan/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Row, Col } from 'react-styled-flexboxgrid'
 
 // Components
-import { Card, Button } from 'Components/Common'
+import { Button } from 'Components/Common'
 
 // Icons
-import CheckIcon from 'Assets/Icons/green-check.svg'
 
 // CSS
 import './SubscriptionPlan.scss'
@@ -16,53 +14,18 @@ import './SubscriptionPlan.scss'
 import { makeFilterSubscriptions } from 'Helpers/Selectors'
 import { planFactory } from 'Helpers'
 
-const initialStyle = {
-  height: '380px',
-  width: '260px',
-  position: 'relative',
-  margin: '0 20px',
-  transition: 'all 200ms ease'
-}
-
 class SubscriptionPlan extends React.Component {
-  state = {
-    style: initialStyle
-  }
-
   selectPlan = () => {
     this.props.select(this.props.plan)
   }
 
-  handleMouseOver = () => {
-    this.setState({
-      style: {
-        ...this.state.style,
-        boxShadow: '0 4px 8px -2px rgba(9, 30, 66, .28), 0 0 1px rgba(9, 30, 66, .3)'
-      }
-    })
-  }
-
-  handleMouseOut = () => {
-    this.setState({ style: initialStyle })
-  }
-
   renderButton () {
     const { subscription, plan, updatingSubscription, creatingSubscription } = this.props
-    const buttonStyle = {
-      position: 'absolute',
-      bottom: '20px',
-      left: '50%',
-      transform: 'translate(-50%, 0)',
-      width: '85%'
-    }
 
     if (subscription && subscription.plan.id === plan) {
       return (
-        <p
-          style={{ ...buttonStyle, bottom: '30px', cursor: 'default' }}
-          className="semibold"
-        >
-          You are currently on this plan
+        <p className="semibold">
+          Subscribed
         </p>
       )
     }
@@ -70,7 +33,6 @@ class SubscriptionPlan extends React.Component {
     if (planFactory[plan].disabled) {
       return (
         <Button
-          style={buttonStyle}
           disabled
           shouldFitContainer
         >
@@ -81,14 +43,13 @@ class SubscriptionPlan extends React.Component {
 
     if (updatingSubscription || creatingSubscription) {
       return (
-        <Button style={buttonStyle} shouldFitContainer loading />
+        <Button shouldFitContainer loading />
       )
     }
 
     return (
       <Button
         onClick={this.selectPlan}
-        style={buttonStyle}
         type="button"
         shouldFitContainer
       >
@@ -108,71 +69,45 @@ class SubscriptionPlan extends React.Component {
   }
 
   render () {
-    const { plan } = this.props
+    const { plan, active } = this.props
 
     return (
-      <Card
-        style={{
-          display: 'inline-block',
-          margin: '0'
-        }}
-        wrapperStyle={{
-          ...this.state.style,
-          opacity: planFactory[plan].disabled ? '0.8' : '1',
-          border: `2px solid ${planFactory[plan].color}`
-        }}
-        onMouseOver={this.handleMouseOver}
-        onMouseOut={this.handleMouseOut}
+      <div
+        styleName={active === plan ? 'subscription-plan active' : 'subscription-plan'}
       >
-        <div styleName="subscription-plan">
-          {this.renderRibbon()}
+        {this.renderRibbon()}
 
-          <Row center='xs'>
-            <Col xs={12}>
-              <p
-                className="semibold small"
-                style={{ color: planFactory[plan].color }}
-              >
-                {plan.split('-')[0].toUpperCase()}
-              </p>
-            </Col>
+        <p
+          style={{ color: planFactory[plan].color }}
+          styleName="plan"
+        >
+          {plan.split('-')[0].toUpperCase()}
+        </p>
 
-            <Col xs={12}>
-              <div styleName="price">
-                <span className="semibold" styleName="dollar">$</span>
-                <span className="semibold" styleName="amount">{planFactory[plan].price}</span>
-                <span className="small" style={{ letterSpacing: '0.5px' }}>/mo</span>
-              </div>
-            </Col>
-          </Row>
-
-          <hr
-            style={{
-              borderBottom: `1px solid ${planFactory[plan].color}`
-            }}
-          />
-
-          <div styleName="features" center='xs'>
-            {
-              planFactory[plan].features.map(feature => (
-                <Row key={feature} center='xs' middle='xs' style={{ marginBottom: '10px' }}>
-                  <Col xs={3}>
-                    <CheckIcon height={10} width={10} style={{ marginRight: '10px' }} />
-                  </Col>
-
-                  <Col xs={9}>
-                    <p style={{ textAlign: 'left' }} className="small">{feature}</p>
-                  </Col>
-                </Row>
-              ))
-            }
-          </div>
-
-          <Row>
-            {this.renderButton()}
-          </Row>
+        <div styleName="price">
+          {
+            plan === 'trial' ? (
+              <span styleName="amount" className="semibold">{planFactory[plan].price}</span>
+            ) : (
+              [
+                <span key="dollar" className="semibold" styleName="dollar">$</span>,
+                <span key="amount" className="semibold" styleName="amount">{planFactory[plan].price}</span>,
+                <span key="mo" className="small" style={{ letterSpacing: '0.5px' }}>/mo</span>
+              ]
+            )
+          }
         </div>
-      </Card>
+
+        <div styleName="features-container">
+          {
+            planFactory[plan].features.map(feature => (
+              <p key={feature}>{feature}</p>
+            ))
+          }
+        </div>
+
+        {this.renderButton()}
+      </div>
     )
   }
 }
@@ -181,7 +116,8 @@ SubscriptionPlan.defaultProps = {
   select: () => null,
   subscription: null,
   creatingSubscription: false,
-  updatingSubscription: false
+  updatingSubscription: false,
+  active: false
 }
 
 SubscriptionPlan.propTypes = {
@@ -189,7 +125,8 @@ SubscriptionPlan.propTypes = {
   select: PropTypes.func,
   subscription: PropTypes.object,
   creatingSubscription: PropTypes.bool,
-  updatingSubscription: PropTypes.bool
+  updatingSubscription: PropTypes.bool,
+  active: PropTypes.bool
 }
 
 const makeMapStateToProps = () => {
