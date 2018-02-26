ea263cfcd387d77da70df1be1b3bb1a48c42eca5
Grant Kang <g1k05j@gmail.com>
2018-02-26 11:40:14 -0800

    Adds mobile media queries

--- src/Components/Help/index.js
+++ src/Components/Help/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
 // Components
-import { QuartzLink } from 'Components/Common'
-import Faq from './Faq'
-import Tos from './Tos'
+import { QuartzLink, Dropdown, MenuItem } from 'Components/Common'
+import { Faq, Tos } from './HelpBlocks'
 import SideNav from './SideNav'
 
 // Icons
@@ -13,50 +13,62 @@ import QuartzIcon from 'Assets/Icons/blue-q-1.svg'
 // CSS
 import './Help.scss'
 
-class Help extends React.Component {
-  render () {
-    return (
-      <main styleName="help-section">
-        <header styleName="header">
-          <div styleName="left">
-            <QuartzIcon
-              width={30}
-              height={30}
-              style={{ marginRight: '20px' }}
-            />
+const Help = ({ history }) => (
+  <main styleName="help-section">
+    <header styleName="header">
+      <div styleName="left">
+        <QuartzIcon
+          width={30}
+          height={30}
+          style={{ marginRight: '20px' }}
+        />
 
-            <QuartzLink to={{ pathname: '/' }}>
-              <span>Quartz</span>
-            </QuartzLink>
-          </div>
+        <QuartzLink to={{ pathname: '/' }}>
+          <span>Quartz</span>
+        </QuartzLink>
+      </div>
 
-          <div styleName="right">
-            <QuartzLink to={{ pathname: '/dashboard' }}>
-              <span>
-                Back to Home
-              </span>
-            </QuartzLink>
-          </div>
-        </header>
+      <div styleName="middle">
+        <Dropdown defaultText='How can we help?' wrapperStyle={{ width: '100%' }}>
+          <MenuItem onClick={() => history.push({ pathname: '/help/faq' })}>
+            FAQ
+          </MenuItem>
 
-        <section styleName="help-center">
-          <div styleName="title">
-            <h4 className='semibold label'>Help Center</h4>
-          </div>
+          <MenuItem onClick={() => history.push({ pathname: '/help/tos' })}>
+            Terms of service
+          </MenuItem>
+        </Dropdown>
+      </div>
 
-          <div styleName="body">
-            <SideNav />
+      <div styleName="right">
+        <QuartzLink to={{ pathname: '/dashboard' }}>
+          <span>
+            Back to Home
+          </span>
+        </QuartzLink>
+      </div>
+    </header>
 
-            <Switch>
-              <Route exact path='/help/faq' component={Faq} />
-              <Route exact path='/help/tos' component={Tos} />
-              <Redirect to='/help/faq' />
-            </Switch>
-          </div>
-        </section>
-      </main>
-    )
-  }
+    <section styleName="help-center">
+      <div styleName="title">
+        <h4 className='semibold label'>Help Center</h4>
+      </div>
+
+      <div styleName="body">
+        <SideNav />
+
+        <Switch>
+          <Route exact path='/help/faq' component={Faq} />
+          <Route exact path='/help/tos' component={Tos} />
+          <Redirect to='/help/faq' />
+        </Switch>
+      </div>
+    </section>
+  </main>
+)
+
+Help.propTypes = {
+  history: PropTypes.object.isRequired
 }
 
 export default Help
