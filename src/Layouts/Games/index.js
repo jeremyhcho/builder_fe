22754732c64c8eaceeb1bf248a1379de9a72b6b6
Grant Kang <g1k05j@gmail.com>
2018-02-26 12:57:46 -0800

    Adds loadables

--- src/Layouts/Games/index.js
+++ src/Layouts/Games/index.js
@@ -1,15 +1,11 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import Loadable from 'react-loadable'
 
 // Components
-import Games from 'Components/Games'
-import GameDetails from 'Components/GameDetails'
+import { Loading } from 'Components/Common'
 
-const GamesLayout = () => (
-  <Switch>
-    <Route exact path='/games' component={Games} />
-    <Route path='/games/:id' component={GameDetails} />
-  </Switch>
-)
+const GamesLayout = Loadable({
+  loader: () => import('./GamesRoute'),
+  loading: Loading
+})
 
 export default GamesLayout
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -32,7 +32,7 @@ const config = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    // filename: '[name].[chunkhash].js'
+    filename: '[name].[chunkhash].js',
     filename: '[name].js',
     publicPath: '/'
   },
