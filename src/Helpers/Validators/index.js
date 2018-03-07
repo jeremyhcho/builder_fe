ae9ef3a3d8f29bc55b1c7c2d42651b967de24fe2
Grant Kang <g1k05j@gmail.com>
2018-03-06 18:41:38 -0800

    Adds proper casing for validation messages

--- src/Helpers/Validators/index.js
+++ src/Helpers/Validators/index.js
@@ -1,13 +1,15 @@
+const sentenceCase = (string) => string[0].toUpperCase() + string.substr(1)
+
 export const presence = (value, form, input, name) => (
   value ? null : `${name} is required`
 )
 
 export const minChar = (length) => (value, form, input, name) => (
-  value && value.length < length ? `${name} must be ${length} characters or more` : null
+  value && value.length < length ? `${sentenceCase(name)} must be ${length} characters or more` : null
 )
 
 export const maxChar = (length) => (value, form, input, name) => (
-  value && value.length > length ? `${name} cannot be greater than ${length} characters` : null
+  value && value.length > length ? `${sentenceCase(name)} cannot be greater than ${length} characters` : null
 )
 
 export const email = (value) => {
@@ -19,20 +21,20 @@ export const email = (value) => {
 export const equality = (inputName) => (value, form) => {
   // equality takes in the name(string) of an input
   if (form[inputName] === value) return null
-  return `${inputName}s do not match`
+  return `${sentenceCase(inputName)}s do not match`
 }
 
 export const match = (matchValue) => (value, form, input, name) => {
   if (matchValue === value) {
     return null
   }
-  return `${name}s do not match`
+  return `${sentenceCase(name)}s do not match`
 }
 
 export const minWord = (length) => (value, form, input, name) => {
   const words = value.split(' ')
   if (words.length >= length && words.every(word => word.length)) return null
-  return `${name} must be ${length} words or more`
+  return `${sentenceCase(name)} must be ${length} words or more`
 }
 
 export const zipCode = (value) => {
