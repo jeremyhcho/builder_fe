d4a4fd13c36b233e2dfab63af4367ffbfcf8ae30
Jeremy Cho <hyung.cho@hyfn.com>
2018-03-07 16:37:22 -0800

    Try to fix mobile date issue

--- src/Actions/nba/games/index.js
+++ src/Actions/nba/games/index.js
@@ -13,7 +13,7 @@ export const fetchNBAGames = createRoutine({
   reducerKey: ['nba', 'games'],
   transform: (response) => (
     response.map(game => ({
-      ...game, date: moment.utc(game.date)
+      ...game, date: moment.utc(new Date(game.date))
     }))
   )
 })
